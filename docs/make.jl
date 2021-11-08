587b77ae2b7d313774ca5be5af19ac317cced299
Jun Tian <tianjun.cpp@gmail.com>
2021-11-08 18:15:01 +0800

    sync local change

--- docs/make.jl
+++ docs/make.jl
@@ -1,1 +1,35 @@
+using OpenTelemetryAPI
+using OpenTelemetrySDK
+using OpenTelemetryProto
+using OpenTelemetryExporterOtlpProtoGrpc
 
+using Documenter
+
+open(joinpath(@__DIR__, "..", "README.md"), "r") do f_src
+    open(joinpath(@__DIR__, "src", "index.md"), "w") do f_dest
+        s_dest = read(f_src, String)
+        s_dest = replace(s_dest, "<!-- ```@raw html -->" => "```@raw html")
+        s_dest = replace(s_dest, "<!-- ``` -->" => "```")
+        write(f_dest, s_dest)
+    end
+end
+
+makedocs(
+    modules = [
+        OpenTelemetryAPI
+        OpenTelemetrySDK
+        OpenTelemetryProto
+        OpenTelemetryExporterOtlpProtoGrpc
+    ],
+    format = Documenter.HTML(
+        prettyurls = true,
+        analytics = "G-YWC6SQHSVM",
+        assets = ["assets/favicon.ico"],
+    ),
+    sitename = "OpenTelemetry.jl",
+    linkcheck = !("skiplinks" in ARGS),
+    pages = [
+        "Home" => "index.md",
+        "Design" => "design.md",
+    ]
+)
