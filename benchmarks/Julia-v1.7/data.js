c03d6b1dde172daef95fa7ceaedd192cad0cc520
github-action-benchmark <github@users.noreply.github.com>
2022-01-27 09:14:10 +0000

    add Benchmark Result with Julia v1.7 (julia) benchmark result for 8d32cee2550c137310d5b10298abbac6e3c81cfa

--- benchmarks/Julia-v1.7/data.js
+++ benchmarks/Julia-v1.7/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1638932198017,
+  "lastUpdate": 1643274850065,
   "repoUrl": "https://github.com/oolong-dev/OpenTelemetry.jl",
   "entries": {
     "Benchmark Result with Julia v1.7": [
@@ -40,6 +40,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "Trace/Create Span",
             "value": 8440.2,
+            "unit": "ns",
+            "extra": "gctime=0\nmemory=5184\nallocs=56\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"samples\":10000,\"evals\":5,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "tanmaykm@gmail.com",
+            "name": "Tanmay Mohapatra",
+            "username": "tanmaykm"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "8d32cee2550c137310d5b10298abbac6e3c81cfa",
+          "message": "fix undefined field access in prometheus exporter (#31)\n\nFixes a few undefined field errors while using `OpenTelemetryExporterPrometheus` with `HistogramAgg`.",
+          "timestamp": "2022-01-27T17:12:58+08:00",
+          "tree_id": "4e22f9754ec3bf62a80d46fc41851155f91ba295",
+          "url": "https://github.com/oolong-dev/OpenTelemetry.jl/commit/8d32cee2550c137310d5b10298abbac6e3c81cfa"
+        },
+        "date": 1643274848871,
+        "tool": "julia",
+        "benches": [
+          {
+            "name": "Meter/Update Counter",
+            "value": 2140,
+            "unit": "ns",
+            "extra": "gctime=0\nmemory=576\nallocs=11\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"samples\":10000,\"evals\":10,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
+          },
+          {
+            "name": "Meter/Update Histogram",
+            "value": 3422.222222222222,
+            "unit": "ns",
+            "extra": "gctime=0\nmemory=1328\nallocs=28\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"samples\":10000,\"evals\":9,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
+          },
+          {
+            "name": "Trace/Create Span",
+            "value": 7600,
             "unit": "ns",
             "extra": "gctime=0\nmemory=5184\nallocs=56\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"samples\":10000,\"evals\":5,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
           }
