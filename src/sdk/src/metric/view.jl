12d75e329fc5ac7d117351b93f9037899f7d6c42
Jun Tian <tianjun.cpp@gmail.com>
2022-02-10 01:15:45 +0800

    update docs in sdk

--- src/sdk/src/metric/view.jl
+++ src/sdk/src/metric/view.jl
@@ -53,6 +53,25 @@ struct View{A}
     aggregation::A
 end
 
+"""
+    View(name=nothing;kwargs...)
+
+The `name` support wildcard.
+
+See more details in [the specification](https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/metrics/sdk.md#view).
+
+## Keyword arguments
+
+  - `description = nothing`,
+  - `attribute_keys = nothing`,
+  - `extra_dimensions = StaticAttrs()`,
+  - `aggregation = nothing`,
+  - `instrument_name = nothing`,
+  - `instrument_type = nothing`,
+  - `meter_name = nothing`,
+  - `meter_version = nothing`,
+  - `meter_schema_url = nothing`,
+"""
 function View(
     name = nothing;
     description = nothing,
