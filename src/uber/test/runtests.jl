5452b0123b65a3ec10e1f3e97aa6ad697aba62b9
Jun Tian <tianjun.cpp@gmail.com>
2022-02-13 14:29:47 +0800

    add Test to dependency

--- src/uber/test/runtests.jl
+++ src/uber/test/runtests.jl
@@ -1,11 +1,12 @@
 using Test
+using Downloads
 
 using OpenTelemetryUber
 
 OpenTelemetryUber.init(
     meter_provider = MeterProvider(),
     tracer_provider = TracerProvider(;
-        span_processor = CompositSpanProcessor(SimpleSpanProcessor(ConsoleExporter())),
+        span_processor = CompositSpanProcessor(SimpleSpanProcessor(ConsoleExporter()))
     ),
 )
 
@@ -17,8 +18,8 @@ in_memory_metric_reader()
 
 prometheus_metric_reader = MetricReader(PrometheusExporter())
 
-@test_throws LoadError with_span("test download") do
-    download("https://julialang.org")
-    download("http://localhost:9966")  # Prometheus
-    download("http://localhost:1234")  # throw exception
+@test_throws Downloads.RequestError with_span("test download") do
+    Downloads.download("https://julialang.org")
+    Downloads.download("http://localhost:9966")  # Prometheus
+    Downloads.download("http://localhost:1234")  # throw exception
 end
