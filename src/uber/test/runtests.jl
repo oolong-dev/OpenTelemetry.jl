0f3e994034b8ba2af249f9184734a6570b04a8ee
Jun Tian <tianjun.cpp@gmail.com>
2022-02-13 14:04:49 +0800

    use @test_throws to catch error

--- src/uber/test/runtests.jl
+++ src/uber/test/runtests.jl
@@ -1,9 +1,11 @@
+using Test
+
 using OpenTelemetryUber
 
 OpenTelemetryUber.init(
     meter_provider = MeterProvider(),
     tracer_provider = TracerProvider(;
-        span_processor = CompositSpanProcessor(SimpleSpanProcessor(ConsoleExporter()))
+        span_processor = CompositSpanProcessor(SimpleSpanProcessor(ConsoleExporter())),
     ),
 )
 
@@ -15,7 +17,7 @@ in_memory_metric_reader()
 
 prometheus_metric_reader = MetricReader(PrometheusExporter())
 
-with_span("test download") do
+@test_throws LoadError with_span("test download") do
     download("https://julialang.org")
     download("http://localhost:9966")  # Prometheus
     download("http://localhost:1234")  # throw exception
