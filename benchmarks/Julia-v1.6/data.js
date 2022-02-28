5ab65f8d2bada453d2babac43c4780da23e020fb
github-action-benchmark <github@users.noreply.github.com>
2022-02-28 14:03:21 +0000

    add Benchmark Result with Julia v1.6 (julia) benchmark result for b921c6967211c466614b17243cb9335209b24d55

--- benchmarks/Julia-v1.6/data.js
+++ benchmarks/Julia-v1.6/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1646023382473,
+  "lastUpdate": 1646057001872,
   "repoUrl": "https://github.com/oolong-dev/OpenTelemetry.jl",
   "entries": {
     "Benchmark Result with Julia v1.6": [
@@ -1324,6 +1324,54 @@ window.BENCHMARK_DATA = {
           {
             "name": "Trace/Create Dummy Span",
             "value": 1580,
+            "unit": "ns",
+            "extra": "gctime=0\nmemory=1072\nallocs=17\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"samples\":10000,\"evals\":10,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "tianjun.cpp@gmail.com",
+            "name": "Jun Tian",
+            "username": "findmyway"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b921c6967211c466614b17243cb9335209b24d55",
+          "message": "Move type piracy of Base.schedule from API to SDK (#68)\n\n* move type piracy of Base.schedule from API to SDK\r\n\r\n* fix import",
+          "timestamp": "2022-02-28T22:01:48+08:00",
+          "tree_id": "87ab1e34ec9bb4c403109650a0bda5d640711300",
+          "url": "https://github.com/oolong-dev/OpenTelemetry.jl/commit/b921c6967211c466614b17243cb9335209b24d55"
+        },
+        "date": 1646057000477,
+        "tool": "julia",
+        "benches": [
+          {
+            "name": "Meter/Update Counter",
+            "value": 1780,
+            "unit": "ns",
+            "extra": "gctime=0\nmemory=592\nallocs=11\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"samples\":10000,\"evals\":10,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
+          },
+          {
+            "name": "Meter/Update Histogram",
+            "value": 3588.8888888888887,
+            "unit": "ns",
+            "extra": "gctime=0\nmemory=1552\nallocs=29\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"samples\":10000,\"evals\":9,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
+          },
+          {
+            "name": "Trace/Create Span",
+            "value": 4257,
+            "unit": "ns",
+            "extra": "gctime=0\nmemory=5120\nallocs=49\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"samples\":10000,\"evals\":7,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
+          },
+          {
+            "name": "Trace/Create Dummy Span",
+            "value": 1560,
             "unit": "ns",
             "extra": "gctime=0\nmemory=1072\nallocs=17\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"samples\":10000,\"evals\":10,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
           }
