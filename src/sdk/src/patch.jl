0dc22141bd8935901957d3c24da632792aa6be7e
Panagiotis Georgakopoulos <panagiotis.georgakopoulos@juliacomputing.com>
2023-08-18 13:02:06 +0300

    feat: maintain the same behaviour for older Julias

--- src/sdk/src/patch.jl
+++ src/sdk/src/patch.jl
@@ -3,8 +3,22 @@
 # https://github.com/oolong-dev/OpenTelemetry.jl/issues/32
 #####
 
-function committypepiracy()
-    @eval function Base.schedule(t::Task)
+@static if VERSION >= v"1.10.0-alpha"
+    function committypepiracy()
+        @eval function Base.schedule(t::Task)
+            ctx = current_context()
+            if ctx !== OpenTelemetryAPI.Context()
+                if isnothing(t.storage)
+                    t.storage = IdDict()
+                end
+                t.storage[OpenTelemetryAPI.CONTEXT_KEY] = ctx
+            end
+            Base.enq_work(t)
+        end
+    end
+else
+    committypepiracy() = nothing
+    function Base.schedule(t::Task)
         ctx = current_context()
         if ctx !== OpenTelemetryAPI.Context()
             if isnothing(t.storage)
