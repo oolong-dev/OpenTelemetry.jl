62a8953783735cde945c96972f4fa214441dee4b
Jun Tian <tianjun.cpp@gmail.com>
2024-04-23 16:13:49 +0800

    resolve #107

--- src/api/test/utils.jl
+++ src/api/test/utils.jl
@@ -1,5 +1,6 @@
 @testset "utils" begin
-    @test get_pkg_version(Logging) == VERSION
+    # https://github.com/oolong-dev/OpenTelemetry.jl/issues/107
+    @test get_pkg_version(Logging) >= VERSION
     withenv("OTEL_LOG_LEVEL" => "error") do
         @test OTEL_LOG_LEVEL() == Logging.Error
     end
